6f7752e4ee2822224f1d8df88687180106ae6f6f
Pierre du Plessis <pierre@pcservice.co.za>
2018-10-05 09:30:56 +0200

    Move namespace to SolidWorx

--- src/Symfony/DependencyInjection/RulerExtension.php
+++ src/Symfony/DependencyInjection/RulerExtension.php
@@ -1,9 +1,10 @@
 <?php
+
 /*
  * This file is part of the ruler project.
  *
- * @author     Pierre du Plessis <pdples@gmail.com>
- * @copyright  Copyright (c) 2016
+ * @author     SolidWorx <open-source@solidworx.co>
+ * @copyright  Copyright (c)
  *
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
